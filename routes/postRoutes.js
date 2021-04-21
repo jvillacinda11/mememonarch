c2a7a896d15b91b0809406a7436d93fb73858d2d
jvilla <josevillacinda96@gmail.com>
2021-04-21 07:52:21 -0700

    commented out unused code

--- routes/postRoutes.js
+++ routes/postRoutes.js
@@ -6,32 +6,7 @@ const passport = require('passport')
 
 //                        Locked
 //   shweta
-// jose's edit
-// crowns shouldn't be part of the body because then a user could give themselves however much crowns they want instead of them being given by other users
-// router.post('/posts', passport.authenticate('jwt'), (req, res) => {
-//   Post.create({
-//     title: req.body.title,
-//     body: req.body.body,
-//     author: req.user._id,
 
-//     // crowns: req.body.crowns
-//     // postDate: Date.now
-//   })
-//     .then(post => {
-//       User.findByIdAndUpdate(req.user._id, { $push: { posts: post._id } })
-//         .then(() => {
-//           res.json({
-//             id: post._id,
-//             title: post.title,
-//             body: post.body,
-//             postDate: post.postDate,
-//             author: req.user
-//           })
-//         })
-//         .catch(err => console.log(err))
-//     })
-//     .catch(err => console.log(err))
-// })
 router.post('/posts', passport.authenticate('jwt'), (req, res) => {
   Post.create({
     title: req.body.title,
@@ -69,29 +44,6 @@ router.get('/posts', (req, res) => {
     .catch(err => console.log(err))
 })
 
-//                                      search functions
-//  jose
-//search by title
-// router.get('/posts/searchByTitle/:title', (req, res) => {
-//   Post.find({ title: { "$regex": req.params.title, "$options": "i" }}).exec(function (err, data) {
-//     if (err) { console.log(err) }
-//     res.json(data)
-//   })
-// })
-//search by author id. This is the companion function of the /api/users/search/:username
-// router.get('/posts/searchByAuthor/:author', (req, res) => {
-//   Post.find({author: req.params.author}).exec(function (err, data) {
-//     if(err) {console.log(err)}
-//     res.json(data)
-//   })
-// })
-//search by tags (only one tag at a time)
-// router.get('/posts/searchByTags/:tag', (req, res) => {
-//   Post.find({ tags: { $regex: new RegExp(req.params.tag, "i") }}).exec(function (err, data)  {
-//     if(err){console.log(err)}
-//     res.json(data)
-//   })
-// })
 
 
 
